1a75114e510032501a74722a290d8cb723edaeb7
Игорь Талавыря <igortalavuria@gmail.com>
2021-06-24 15:28:54 +0300

    add info product, card, checkEmp, productImg

--- PythonPC/main.py
+++ PythonPC/main.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from interface import Ui_MainWindow
 from PyQt5.QtCore import QEvent, QThread, pyqtSignal
+from datetime import datetime
 import requests 
 import sys
 import time
@@ -16,6 +17,9 @@ ui.setupUi(MainWindow)
 MainWindow.show()
 
 # Logic
+
+# QThead For Advertising
+
 class AdvertisingWorker(QThread):
 
     change_image = pyqtSignal()
@@ -28,6 +32,8 @@ class AdvertisingWorker(QThread):
                 timing = time.time()
                 ui.change_image = (QtGui.QPixmap("PythonPC/img/advertise/133.Episode.png"))
                 print(ui.change_image)
+
+# QThead For ProgressBar
 
 class ProgressBarWorker(QThread):   
     
@@ -43,6 +49,8 @@ class ProgressBarWorker(QThread):
             if (progress == 0):    
                 print(progress)
                 ui.change_image = QtGui.QPixmap("PythonPC/img/resources/systemInfo_dark.jpg")
+
+# Get System Info
 """        
 def getInfo():
     deviceName = open("/etc/hostname", "r")
@@ -78,6 +86,8 @@ def getInfo():
     ui.humidity.setText("Влажность: ")
     ui.numberBody.setText("Номер корпуса: ")
 """
+# When barcode textChanged
+
 def sync_lineEdit():
     barcode = ui.barcode.text() 
     print(barcode)
@@ -90,8 +100,14 @@ def sync_lineEdit():
     ui.codeValue.setGeometry(QtCore.QRect(460, 550, 0, 0))
     ui.name.setGeometry(QtCore.QRect(80, 320, 0, 0))
     ui.price.setGeometry(QtCore.QRect(250, 200, 0, 0))
-    ui.nameCard.setGeometry(QtCore.QRect(120, 180, 800, 0))
-    ui.bonus.setGeometry(QtCore.QRect(0, 210, 1024, 0))
+    ui.nameCard.setGeometry(QtCore.QRect(120, 180, 0, 0))
+    ui.bonus.setGeometry(QtCore.QRect(0, 210, 0, 0))
+    ui.nameEmp.setGeometry(QtCore.QRect(0, 160, 0, 0))
+    ui.statusEmp.setGeometry(QtCore.QRect(0, 320, 0, 0))
+    ui.timeEmp.setGeometry(QtCore.QRect(0, 400, 0, 0))
+    ui.productImage.setGeometry(QtCore.QRect(530, 30, 0, 0))
+    ui.pricePenny.setGeometry(QtCore.QRect(380, 120, 0, 0))
+    ui.priceCurrency.setGeometry(QtCore.QRect(380, 190, 0, 0))
 
     if (barcode != ""):    
         ui.image.setPixmap(QtGui.QPixmap("PythonPC/img/resources/manualInputBc_dark.png"))
@@ -100,6 +116,8 @@ def sync_lineEdit():
 
     ui.progressBarThread.start() 
     ui.progressBarThread.quit()      
+
+# When Press Enter
 
 def barcodePressedEnter():
 
@@ -148,13 +166,61 @@ def barcodePressedEnter():
         ui.image.setPixmap(QtGui.QPixmap("PythonPC/img/resources/systemInfo_dark.jpg"))
         print("Сброс")
 
-    elif (barcode.find("ent") == False):
-        emp = 'http://mpce03.avrora.lan/emp_info?key=39fa302c1a6b40e19020b376c9becb3b&stock=236&device=DeviceName&barcode='
-        emp += barcode
-        response = requests.get(emp)
-        print(response.text)
+    # Check Employment
+
+    elif (barcode.find("ent") == False):    
+        try: 
+            ui.barcode.setText("")
+            ui.barcode.setGeometry(QtCore.QRect(70, 245, 0, 0))
+            ui.image.setPixmap(QtGui.QPixmap("PythonPC/img/resources/employeeInfo.png"))
+
+            emp = 'http://mpce03.avrora.lan/emp_info?key=39fa302c1a6b40e19020b376c9becb3b&stock=236&device=DeviceName&barcode='
+            emp += barcode
+
+            response = requests.get(emp).text
+            print(response)
+            responseMini1 = response.replace('{"Id":', '')
+            idStr = responseMini1[0:responseMini1.index(',')]
+            responseMini2 = responseMini1.replace(idStr + ',"Barcode":"','')
+            barcodeStr = responseMini2[0:responseMini2.index('",')]
+            responseMini3 = responseMini2.replace(barcodeStr + '","Name":"','')
+            nameEmp = responseMini3[0:responseMini3.index('",')]
+            responseMini4 = responseMini3.replace(nameEmp + '","State":','')
+            state = responseMini4[0:responseMini4.index('}')]
+
+            ui.nameEmp.setWordWrap(True)
+            ui.nameEmp.setText(nameEmp)
+
+            if(state == "0"):
+                ui.statusEmp.setText("кінець")
+            elif(state == "1"):
+                ui.statusEmp.setText("початок")
+            
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            ui.timeEmp.setText("робочого дня зафіксовано о " + current_time)
+
+            ui.nameEmp.setGeometry(QtCore.QRect(0, 150, 1024, 200))
+            ui.statusEmp.setGeometry(QtCore.QRect(0, 310, 1024, 200))
+            ui.timeEmp.setGeometry(QtCore.QRect(0, 380, 1024, 200))
+
+        except Exception:
+
+            ui.barcodeText.setGeometry(QtCore.QRect(40, 550, 0, 0))
+            ui.barcodeValue.setGeometry(QtCore.QRect(80, 550, 0, 0))
+            ui.amountText.setGeometry(QtCore.QRect(290, 550, 0, 0))
+            ui.amountValue.setGeometry(QtCore.QRect(370, 550, 0, 0))
+            ui.codeText.setGeometry(QtCore.QRect(420, 550, 0, 0))
+            ui.codeValue.setGeometry(QtCore.QRect(460, 550, 0, 0))
+            ui.name.setGeometry(QtCore.QRect(80, 320, 0, 0))
+            ui.price.setGeometry(QtCore.QRect(250, 200, 0, 0))
+
+            ui.image.setPixmap(QtGui.QPixmap("PythonPC/img/resources/merchandiserError.png"))          
 
     else:
+
+        # Check Barcode
+
         if (len(barcode) > 12):
             ui.barcode.setText("")
             ui.barcode.setGeometry(QtCore.QRect(70, 245, 0, 0))
@@ -164,6 +230,8 @@ def barcodePressedEnter():
             response = requests.get(art)
             getProductInfo(art,barcode)
 
+        # Check Code
+
         elif(len(barcode) <= 12):
             ui.barcode.setText("")
             ui.barcode.setGeometry(QtCore.QRect(70, 245, 0, 0))
@@ -172,6 +240,7 @@ def barcodePressedEnter():
             art += barcode +'&source=1'
             getProductInfo(art,barcode)
 
+# Info For product 
 
 def getProductInfo(art,barcode):
     try:
@@ -190,6 +259,13 @@ def getProductInfo(art,barcode):
         responseMini6 = responseMini5.replace(priceOldStr + ',"Qty":','')
         qtyStr = responseMini6[0:responseMini6.index('}')]
 
+        priceFloat = float(priceStr)
+        price = str("%.0f" % priceFloat)
+        penny = str("%.0f" % ((priceFloat%1) * 100) )
+
+        if (penny == "0"):
+            penny = "00"
+
         ui.barcodeText.setText("Ш/К:")
         ui.barcodeValue.setText(barcodeStr)
         ui.amountText.setText("Кількість:")
@@ -197,18 +273,56 @@ def getProductInfo(art,barcode):
         ui.codeText.setText("Код:")
         ui.codeValue.setText(idStr)
         ui.name.setText(nameStr)
-        ui.price.setText(priceStr)
+        ui.price.setText(price)
+        ui.pricePenny.setText(penny)
+        ui.priceCurrency.setText("грн")
 
         ui.barcodeText.setGeometry(QtCore.QRect(40, 550, 80, 20))
         ui.barcodeValue.setGeometry(QtCore.QRect(80, 550, 200, 20))
-        ui.amountText.setGeometry(QtCore.QRect(290, 550, 100, 20))
-        ui.amountValue.setGeometry(QtCore.QRect(370, 550, 80, 20))
-        ui.codeText.setGeometry(QtCore.QRect(420, 550, 80, 20))
-        ui.codeValue.setGeometry(QtCore.QRect(460, 550, 80, 20))
-        ui.name.setGeometry(QtCore.QRect(80, 320, 400, 200))
-        ui.price.setGeometry(QtCore.QRect(250, 200, 80, 20))
+        ui.amountText.setGeometry(QtCore.QRect(270, 550, 100, 20))
+        ui.amountValue.setGeometry(QtCore.QRect(350, 550, 80, 20))
+        ui.codeText.setGeometry(QtCore.QRect(400, 550, 80, 20))
+        ui.codeValue.setGeometry(QtCore.QRect(440, 550, 80, 20))
+        ui.name.setWordWrap(True)
+        ui.name.setGeometry(QtCore.QRect(40, 380, 450, 180))
+        
+
+        if (priceFloat < 10):
+            ui.price.setGeometry(QtCore.QRect(0, 100, 450, 300))
+            ui.pricePenny.setGeometry(QtCore.QRect(300, 120, 200, 200))
+            ui.priceCurrency.setGeometry(QtCore.QRect(300, 190, 200, 200))
+        elif (priceFloat < 100):
+            ui.price.setGeometry(QtCore.QRect(0, 100, 450, 300))
+            ui.pricePenny.setGeometry(QtCore.QRect(350, 120, 200, 200))
+            ui.priceCurrency.setGeometry(QtCore.QRect(350, 190, 200, 200))  
+        elif (priceFloat >= 100):      
+            ui.price.setGeometry(QtCore.QRect(0, 100, 400, 300))
+            ui.pricePenny.setGeometry(QtCore.QRect(380, 120, 200, 200))
+            ui.priceCurrency.setGeometry(QtCore.QRect(380, 190, 200, 200))
+
+        # Get image product
+
+        try:
+            image  = 'http://mpce04.avrora.lan/img?key=39fa302c1a6b40e19020b376c9becb3b&stock=235&device=DeviceName&code='
+            image += idStr + '&sticker=1'
+            
+            responseImage = requests.get(image)
+            
+            print(responseImage.url)
+            file = open("PythonPC/img/temp/temp_image.png", "wb")
+            file.write(responseImage.content)
+            file.close()
+
+            ui.productImage.setStyleSheet("border-radius:15px; border-image: url(PythonPC/img/temp/temp_image.png) 0 0 0 0 stretch stretch;")
+            
+            ui.productImage.setGeometry(QtCore.QRect(530, 25, 470, 545))
+                
+        except Exception:
+            ui.productImage.setPixmap(QtGui.QPixmap("PythonPC/img/resources/noImage.jpg"))         
 
     except Exception:
+
+        # Check Card
 
         try:
             card = 'http://mpce04.avrora.lan/card?key=39fa302c1a6b40e19020b376c9becb3b&stock=235&device=DeviceName&card='
@@ -222,9 +336,13 @@ def getProductInfo(art,barcode):
             idStr = responseMini2Card[0:responseMini2Card.index('",')]
             responseMini3Card = responseMini2Card.replace(idStr + '","Name":"' ,'')
             nameStrCard = responseMini3Card[0:responseMini3Card.index('"}')]
+
+            bonus = int(bonusStr)
+            cash = bonus / 100
+            penny = bonus % 100
             
             ui.nameCard.setText(nameStrCard + ', на Вашому рахунку')
-            ui.bonus.setText(bonusStr)
+            ui.bonus.setText(str("%.0f" % cash) + "." + str(penny))
 
             ui.nameCard.setGeometry(QtCore.QRect(120, 180, 800, 60))
             ui.bonus.setGeometry(QtCore.QRect(0, 210, 1024, 400))
